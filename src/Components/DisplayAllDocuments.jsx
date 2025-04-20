7443eecbfa2f27b9155abedae88a9494a3e0c5e3
Dávid Oliver Spirczak <xspirczak@stuba.sk>
2025-04-20 23:44:32 +0200

    fixed background color of loading (document search page)

--- src/Components/DisplayAllDocuments.jsx
+++ src/Components/DisplayAllDocuments.jsx
@@ -276,7 +276,7 @@ export default function DisplayAllDocuments() {
 
     if (loading) {
         return (
-            <div className="flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-cyan-200">
+            <div className="flex justify-center items-center h-screen">
                 <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-blue-500"></div>
             </div>
         );
